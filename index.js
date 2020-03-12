6ac3696d97d994623368223f7f8b1d7ecf511a57
Daria Lazovskaya <32599716+odasco@users.noreply.github.com>
2020-03-12 23:32:12 +0300

    second

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send({mes: "HI"})
+    res.send({mes: "Dima gei"})
 });
 
 const PORT = process.env.PORT || 5000;
