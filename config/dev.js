24f7c82b3e4ce6358157c9506b79b98179a95591
EXT02D18761 <darya.lazouskaya@consultant.turkcell.com.tr>
2020-03-19 14:21:50 +0300

    finished 5 section

---
+++ config/dev.js
@@ -0,0 +1,6 @@
+module.exports = {
+    googleClientID: '949888967380-eipdd8gaqfjmrj03lanibdujbg9k0p9a.apps.googleusercontent.com',
+    gooogleClientSecret: '4o6r9cw11ZiNzDTRCBOnq31Z',
+    mongoURI: 'mongodb+srv://user:user@emaily-wrlfo.mongodb.net/test?retryWrites=true&w=majority',
+    cookieKey: 'tyuyioiriir'
+};
--- config/keys.js
+++ config/keys.js
@@ -1,6 +1,5 @@
-module.exports = {
-    googleClientID: '949888967380-eipdd8gaqfjmrj03lanibdujbg9k0p9a.apps.googleusercontent.com',
-    gooogleClientSecret: '4o6r9cw11ZiNzDTRCBOnq31Z',
-    mongoURI: 'mongodb+srv://user:user@emaily-wrlfo.mongodb.net/test?retryWrites=true&w=majority',
-    cookieKey: 'tyuyioiriir'
-};
+if (process.env.NODE_ENV === 'production') {
+    module.exports = require('./prod');
+} else {
+    module.exports = require('./dev');
+}
